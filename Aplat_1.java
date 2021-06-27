1976269736dd743d6f568df697d18f2a9d04ce23
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-06-27 17:39:31 +0530

    Add files via upload

---
+++ Aplat_1.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Aplat_1
+ {
+
+	public static void main(String[] args) {
+      String input = (String)JOptionPane.showInputDialog(null, "Is this a Question ?",
+    		  "Dialog Title",
+    		  2, 
+    		  null,
+    		  null,
+    		  "Type something here.");
+   }
+}
